c99d83a52f4146c6ebb8cf08bb345f38b104d81e
Adrian Grzemski <adrian.grzemski@gmail.com>
2023-06-17 22:20:16 +0200

    added test for files

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,17 @@
 mod input_source;
 
-use input_source::InputSource;
 use clap::Parser;
-use serde_json:: Value;
+use input_source::InputSource;
+use serde_json::Value;
+
+use log::*;
+use std::str::FromStr;
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
 struct Cli {
-    #[arg(help = "Input file")]
-    file: Option<String>,
+    #[arg(short, long, help = "Input file")]
+    input: Option<String>,
 
     #[arg(short, long, help = "Elements to be selected")]
     select: Option<String>,
@@ -17,16 +20,28 @@ struct Cli {
     filter: Option<String>,
 
     #[arg(short, long, help = "Shows additional debug info")]
-    debug: bool
+    debug: bool,
+
+    #[arg(short, long, action = clap::ArgAction::Count)]
+    verbosity: u8,
 }
 fn main() {
+    info!("Start rjp");
     let cli = Cli::parse();
+    let verbose = std::cmp::min(cli.verbosity, 4) as usize;
+    let ts = stderrlog::Timestamp::from_str("ms").unwrap();
 
-    if cli.debug {
-        eprintln!("{:#?}", cli)
-    }
+    stderrlog::new()
+        .module(module_path!())
+        .quiet(false)
+        .verbosity(verbose)
+        .timestamp(ts)
+        .init()
+        .unwrap();
 
-    let mut input_source = if let Some(path) = cli.file {
+    debug!("{:#?}", cli);
+
+    let mut input_source = if let Some(path) = cli.input {
         InputSource::new_file(&path).expect(&format!("Error reading {}", &path))
     } else {
         InputSource::new_stdin()
@@ -34,16 +49,18 @@ fn main() {
 
     let mut all_lines = String::new();
     loop {
-        let line = input_source
-            .lines()
-            .expect("Error reading line");
+        let line = input_source.lines().expect("Error reading line");
         if line.is_empty() {
             break;
         }
         all_lines.push_str(&line);
     }
 
+    debug_assert!(all_lines.len() > 0);
+
     let json_content: Value = serde_json::from_str(&all_lines).expect("");
 
+    info!("Printing output");
     println!("{}", serde_json::to_string_pretty(&json_content).unwrap())
+
 }
--- tests/cli.rs
+++ tests/cli.rs
@@ -26,12 +26,16 @@ fn prints_stdin() -> TestResult {
 fn run_file_success(input_path: &str, args: &[&str]) -> TestResult {
     let input_file = format!("{}/{}", input_path, "input.json");
     let expected_file = format!("{}/{}", input_path, "expected.json");
-    let expected = fs::read_to_string(expected_file).unwrap();
+    let expected = fs::read_to_string(&expected_file).expect("Failed to read expected file");
+    let input = fs::read_to_string(&input_file).expect("Failed to read input file");
+
+    assert!(expected.len() > 0);
+    assert!(input.len() > 0);
 
     Command::cargo_bin(CMD)
         .unwrap()
         .args(args)
-        .args(&["-f", &input_file])
+        .args(&["-d", "-i", &input_file])
         .assert()
         .success()
         .stdout(expected.to_owned());
