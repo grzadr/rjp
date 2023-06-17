b3c7b092e1cb1ae1d2f2859b1ff7bc6d2a37f154
Adrian Grzemski <adrian.grzemski@gmail.com>
2023-06-17 13:51:30 +0200

    Non working testing files

--- tests/cli.rs
+++ tests/cli.rs
@@ -1,4 +1,5 @@
 use assert_cmd::Command;
+use std::fs;
 
 type TestResult = Result<(), Box<dyn std::error::Error>>;
 
@@ -16,7 +17,28 @@ fn run_stdin(args: &[&str], stdin: &str, expected: &str) -> TestResult {
     Ok(())
 }
 
+
 #[test]
 fn prints_stdin() -> TestResult {
     run_stdin(&[], "{}", "{}\n")
+}
+
+fn run_file_success(input_path: &str, args: &[&str]) -> TestResult {
+    let input_file = format!("{}/{}", input_path, "input.json");
+    let expected_file = format!("{}/{}", input_path, "expected.json");
+    let expected = fs::read_to_string(expected_file).unwrap();
+
+    Command::cargo_bin(CMD)
+        .unwrap()
+        .args(args)
+        .args(&["-f", &input_file])
+        .assert()
+        .success()
+        .stdout(expected.to_owned());
+    Ok(())
+}
+
+#[test]
+fn test_empty_file() -> TestResult {
+    run_file_success("tests/json_files/empty", &[])
 }
